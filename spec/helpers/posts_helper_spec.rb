e29a8a05983a780f06f332270dc72c1506d54e72
Devan Kestel <devan.kestel@gmail.com>
2021-11-28 21:12:47 -0500

    Add branches request spec.

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -41,14 +41,12 @@ RSpec.describe PostsHelper, type: :helper do
   end
   context "#no_posts_partial_path" do 
     it "returns a no_posts partial's path" do
-      assign(:posts, [])
-      expect(helper.no_posts_partial_path). to(
-        eq 'posts/branch/no_posts'
+      expect(helper.no_posts_partial_path([])). to(
+        eq 'posts/shared/no_posts'
       ) 
     end
     it "returns an empty_partial partial's path" do
-      assign(:posts, [1])
-      expect(helper.no_posts_partial_path). to(
+      expect(helper.no_posts_partial_path([1])). to(
         eq 'shared/empty_partial'
       ) 
     end
