f68b75a558adbc7594d6907fa668536213dbb29d
Devan Kestel <devan.kestel@gmail.com>
2021-12-12 00:41:51 -0500

    WIP conversation.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,10 @@ class PostsController < ApplicationController
     end
     
     def show
-        @post = Post.find(params[:id]) 
+        @post = Post.find(params[:id])
+        if user_signed_in?
+            @message_has_been_sent = conversation_exist?
+        end 
     end
 
     def study
@@ -55,5 +58,9 @@ class PostsController < ApplicationController
             category: params[:category]
 
         }).call
+    end
+
+    def conversation_exist?
+        Private::Conversation.between_users(current_user.id, @post.user.id).present?
     end
 end
