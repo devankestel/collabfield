100225ecdbec64c5d07ad320f86aa0f6721ced48
Devan Kestel <devan.kestel@gmail.com>
2021-12-12 20:42:56 -0500

    Passing scope test on convo between users.

--- app/models/private/conversation.rb
+++ app/models/private/conversation.rb
@@ -5,7 +5,7 @@ class Private::Conversation < ApplicationRecord
              class_name: "Private::Message",
              foreign_key: :conversation_id
     belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
-    belongs_to :recipient, foreign_key: :recipient_id, class_name 'User'
+    belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
 
     scope :between_users, -> (user1_id, user2_id) do
         where(sender_id: user1_id, recipient_id: user2_id).or(
